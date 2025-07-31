ddc277774100aab17ed5fe3febb9f7a20db98139
henrick.nogueira <henrick.nogueira@cnpq.br>
2025-07-31 17:29:48 -0300

    Criando um personagem

--- snake.py
+++ snake.py
@@ -2,10 +2,24 @@ import curses
 import time
 
 def game_loop(window):
-    for i in range(10):
-        window.addstr(f'O valor de i é {i}\n')
-        window.refresh()
-        time.sleep(1)
+    personagem = [10, 15]
+    window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
+    while True:
+        window.timeout(1000)
+        char = window.getch()
+        window.clear()
+        match char:
+            case curses.KEY_UP:
+                personagem[0] -= 1
+            case curses.KEY_LEFT:
+                personagem[1] -= 1
+            case curses.KEY_DOWN:
+                personagem[0] += 1
+            case curses.KEY_RIGHT:
+                personagem[1] += 1
+            case _:
+                pass
+        window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
 
 if __name__ == '__main__':
     curses.wrapper(game_loop)
