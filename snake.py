3f42ebfde5884168dbe48c61f3d334ba00293d86
henrick.nogueira <henrick.nogueira@cnpq.br>
2025-08-02 14:06:36 -0300

    Criando a cobrinha

--- snake.py
+++ snake.py
@@ -1,40 +1,42 @@
 import curses
-from asyncio import timeout
 
 
 def game_loop(window):
     # Setup inicial
     curses.curs_set(0)
-    personagem = [10, 15]
+    snake = [
+        [12, 15],  # Cabeça
+        [11, 15],
+        [10, 15],  # Cauda
+    ]
     current_direction = curses.KEY_DOWN
 
+    # Loop do jogo
     while True:
-        draw_screen(window)
-        draw_actor(actor=personagem, window=window)
+        draw_screen(window=window)
+        draw_snake(snake=snake, window=window)
         direction = get_new_direction(window=window, timeout=1000)
         if direction is None:
             direction = current_direction
-        move_actor(actor=personagem, direction=direction)
-        if actor_hit_border(actor=personagem, window=window):
+        move_snake(snake=snake, direction=direction)
+        if snake_hit_border(snake=snake, window=window):
             return
         current_direction = direction
-
-
-def draw_screen(window):
-    window.clear()
-    window.border(0)
-
-
-def draw_actor(actor, window):
-    window.addch(actor[0], actor[1], curses.ACS_DIAMOND)
 
 
 def get_new_direction(window, timeout):
     window.timeout(timeout)
     direction = window.getch()
-    if direction in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT]:
+    if direction in [curses.KEY_UP, curses.KEY_LEFT, curses.KEY_DOWN, curses.KEY_RIGHT]:
         return direction
     return None
+
+
+def move_snake(snake, direction):
+    head = snake[0].copy()
+    snake.insert(0, head)
+    move_actor(actor=head, direction=direction)
+    snake.pop()
 
 
 def move_actor(actor, direction):
@@ -47,18 +49,39 @@ def move_actor(actor, direction):
             actor[0] += 1
         case curses.KEY_RIGHT:
             actor[1] += 1
-        case _:
-            pass
+
+
+def snake_hit_border(snake, window):
+    head = snake[0]
+    return actor_hit_border(actor=head, window=window)
 
 
 def actor_hit_border(actor, window):
     height, width = window.getmaxyx()
-    if (actor[0] <= 0) or (actor[0] >= height - 1):
+    # EIXO VERTICAL
+    if (actor[0] <= 0) or (actor[0] >= (height - 1)):
         return True
-    if (actor[1] <= 0) or (actor[1] >= width - 1):
+    # EIXO HORIZONTAL
+    if (actor[1] <= 0) or (actor[1] >= (width - 1)):
         return True
-    window.addch(actor[0], actor[1], curses.ACS_DIAMOND)
     return False
+
+
+def draw_screen(window):
+    window.clear()
+    window.border(0)
+
+
+def draw_snake(snake, window):
+    head = snake[0]
+    body = snake[1:]
+    draw_actor(actor=head, window=window, char="@")
+    for body_part in body:
+        draw_actor(actor=body_part, window=window, char="s")
+
+
+def draw_actor(actor, window, char):
+    window.addch(actor[0], actor[1], char)
 
 
 if __name__ == '__main__':
